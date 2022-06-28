8d55b000de24c386ef499f0efea4742560987ac2
Adaeze Ndupu <andupu4@gmail.com>
2022-06-28 03:43:14 +0100

    Easter egg

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a list has a cycle
+ * @list: pointer to the head of the list
+ * Return: 1 if there is a cycle, 0 if there is not
+ */
+
+int check_cycle(listint_t *list)
+{
+    listint_t *slow;
+    listint_t *fast;
+
+    if (list == NULL)
+        return (0);
+    slow = list;
+    fast = list;
+    while (fast->next != NULL && fast->next->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+            return (1);
+    }
+    return (0);
+}
