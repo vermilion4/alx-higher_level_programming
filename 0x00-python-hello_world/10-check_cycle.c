63b87e80f4a54d719eed0b7625b9e916c6ce3cb4
Adaeze Ndupu <andupu4@gmail.com>
2022-06-28 03:45:01 +0100

    betty fix

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,19 +11,19 @@
 
 int check_cycle(listint_t *list)
 {
-    listint_t *slow;
-    listint_t *fast;
+listint_t *slow;
+listint_t *fast;
 
-    if (list == NULL)
-        return (0);
-    slow = list;
-    fast = list;
-    while (fast->next != NULL && fast->next->next != NULL)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
-        if (slow == fast)
-            return (1);
-    }
-    return (0);
+if (list == NULL)
+return (0);
+slow = list;
+fast = list;
+while (fast->next != NULL && fast->next->next != NULL)
+{
+slow = slow->next;
+fast = fast->next->next;
+if (slow == fast)
+return (1);
+}
+return (0);
 }
