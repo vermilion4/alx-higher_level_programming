924f14949088c6b029bd4dddfff529d73562ecb3
Adaeze Ndupu <andupu4@gmail.com>
2022-07-07 01:03:24 +0100

    replace removed

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    chars = 'Cc'
-    for char in chars:
-        my_string = my_string.replace(char, '')
-    return my_string
+    new_string = my_string.translate(str.maketrans('', '', 'cC'))
+    return new_string
