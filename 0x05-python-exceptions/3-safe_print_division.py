a6b2ae495d6f06fe388defea7a216faed10cf156
Adaeze Ndupu <andupu4@gmail.com>
2022-07-24 21:15:13 +0100

    python exceptions

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    """Returns the division of a by b."""
+    try:
+        division = a / b
+    except (TypeError, ZeroDivisionError):
+        division = None
+    finally:
+        print("Inside result: {}".format(division))
+    return (division)
